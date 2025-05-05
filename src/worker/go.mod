b5f9be84156f1bc98992387b69524d93f2002643
RandomProjects-db <RandomProjects-db@hotmail.com>
2025-05-05 19:13:18 +0300

    Added Worker + go file

--- src/worker/go.mod
+++ src/worker/go.mod
@@ -1,3 +1,10 @@
-module github.com/RandomProjects-db/url-shortener-devops-project/worker
+module github.com/your-username/url-shortener-devops-project/worker
 
-go 1.24.2
+go 1.21
+
+require github.com/redis/go-redis/v9 v9.5.1
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
--- src/worker/main.go
+++ src/worker/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	cleanupInterval = 1 * time.Hour
+	keyPrefix       = "url:"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379", // Matches compose service name
+		Password: "",
+		DB:       0,
+	})
+
+	for {
+		cleanupExpiredURLs(rdb)
+		time.Sleep(cleanupInterval)
+	}
+}
+
+func cleanupExpiredURLs(rdb *redis.Client) {
+	ctx := context.Background()
+	keys, err := rdb.Keys(ctx, keyPrefix+"*").Result()
+	if err != nil {
+		log.Printf("Error fetching keys: %v", err)
+		return
+	}
+
+	for _, key := range keys {
+		if ttl, err := rdb.TTL(ctx, key).Result(); err == nil {
+			if ttl < 0 { // -1 = no expiration, -2 = key doesn't exist
+				rdb.Del(ctx, key)
+				log.Printf("Cleaned up key: %s", key)
+			}
+		}
+	}
+	fmt.Printf("Cleanup completed at %s\n", time.Now().Format(time.RFC3339))
 }
